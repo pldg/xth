e85186c1615e0a6507cc31a848ce8847b69d793d
pldg <luca.poldelmengo@gmail.com>
2019-02-20 20:28:26 +0100

    Create simple app to test xth

--- index.js
+++ index.js
@@ -1,90 +1,66 @@
-/*! MIT (c) Luca Poldelmengo | https://github.com/pldg/xth */
+const inputXml = document.getElementById('select-xml');
+const inputXsl = document.getElementById('select-xsl');
+const selectedXml = document.getElementById('selected-xml');
+const selectedXsl = document.getElementById('selected-xsl');
+const previewToIframe = document.getElementById('preview-to-iframe');
+const writeToPage = document.getElementById('write-to-page');
+const iframe = document.querySelector('iframe');
+const files = [];
 
-/**
- * Xml-To-Html
- * Transform XML/XSLT to HTML
- * Compatible with IE 10+
- * @param {String} xmlPath Path to XML file
- * @param {String} xslPath Path to XSL file
- * @param {Function} callback Handle transformed HTML
- */
-function xth(xmlPath, xslPath, callback) {
-  var isIE = detectIE();
+inputXml.addEventListener('change', handleFile);
+inputXsl.addEventListener('change', handleFile);
+previewToIframe.addEventListener('click', handleConversion);
+writeToPage.addEventListener('click', handleConversion);
 
-  loadDocument(xmlPath, function(xml) {
-    loadDocument(xslPath, function(xsl) {
-      var html = '<!DOCTYPE html>' + transformXml(xml, xsl);
-      callback(html);
-    });
-  });
+// Push uploaded file into files array
+function handleFile() {
+  const fileList = this.files;
+  const file = fileList[0];
 
-  /**
-   * Parse XML/XSL files and transform to HTML
-   * @param {Object} xml XML document
-   * @param {Object} xsl XSL document
-   * @returns {String} Transformed HTML
-   */
-  function transformXml(xml, xsl) {
-    var html;
-
-    if (isIE) {
-      html = xml.transformNode(xsl);
-    } else {
-      var xsltProcessor = new XSLTProcessor();
-      xsltProcessor.importStylesheet(xsl);
-      var xmlDom = xsltProcessor.transformToDocument(xml);
-
-      var serializer = new XMLSerializer();
-      html = serializer.serializeToString(xmlDom.documentElement);
-    }
-
-    return html;
+  // Display the current uploaded file next to its input field
+  if (this.id === 'select-xml') {
+    selectedXml.textContent = file.name;
+  } else {
+    selectedXsl.textContent = file.name;
   }
 
-  /**
-   * Use XHR to load "document" type files (HTML/XML/XSL)
-   * @param {String} filepath Path to document
-   * @param {Function} callback Handle loaded document
-   */
-  function loadDocument(filepath, callback) {
-    var xhr;
-
-    if (isIE) xhr = new ActiveXObject('Microsoft.XMLHTTP');
-    else xhr = new XMLHttpRequest();
-
-    if (!xhr) {
-      throw 'ERROR: No AJAX support';
-    } else {
-      xhr.open('GET', filepath);
-      if (!isIE) xhr.responseType = 'document';
-      xhr.send();
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            if (isIE) callback(xhr.responseXML);
-            else callback(xhr.response);
-          } else {
-            throw 'ERROR: Could not load: ' + filepath;
-          }
-        }
-      }
-    }
-  }
-
-  /**
-   * Detect Internet Explorer
-   * @returns {True|False} If it's IE return `true` otherwise return `false`
-   * {@link https://stackoverflow.com/a/21712356/}
-   */
-  function detectIE() {
-    var ua = window.navigator.userAgent;
-    var msie = ua.indexOf('MSIE ');
-    var trident = ua.indexOf('Trident/');
-    if (msie > 0 || trident > 0) {
-      return true;
-    }
-    return false;
-  }
+  files.push(file);
 }
 
-module.exports = xth;
+// Read uploaded XML and XSL files
+// and convert them to HTML
+function handleConversion() {
+  const warning = document.getElementById('warning');
+  const warningMessage = '<p id="warning">Make sure both XML and XSL files are selected</p>';
+  const readXml = new FileReader();
+  const readXsl = new FileReader();
+
+  try {
+    readXml.readAsDataURL(files[0]);
+    readXsl.readAsDataURL(files[1]);
+    if (warning !== null) warning.parentNode.removeChild(warning);
+  } catch (error) {
+    if (warning === null) {
+      document.body.insertAdjacentHTML('afterbegin', warningMessage);
+    }
+  }
+
+  readXml.onload = () => {
+    readXsl.onload = () => {
+      const xml = readXml.result;
+      const xsl = readXsl.result;
+
+      // Convert XML-XSL to HTML
+      xth(xml, xsl, (html) => {
+        if (this.id === 'preview-to-iframe') {
+          // Preview converted HTML to iframe
+          iframe.src = 'data:text/html;charset=UTF-8,' + encodeURIComponent(html);
+        } else {
+          // Write converted HTML to page
+          // so you can stamp the page if needed
+          document.write(html);
+        }
+      });
+    }
+  }
+}
--- test/index.js
+++
@@ -1,8 +0,0 @@
-var xml = './index.xml';
-var xsl = './style.xsl';
-
-xth(xml, xsl, function(html) {
-  console.log(html);
-  var iframe = document.querySelector('iframe');
-  iframe.src = 'data:text/html;charset=UTF-8,' + encodeURIComponent(html);
-});
